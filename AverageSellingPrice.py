53d7091acf1d4aa856d756c519fb5985758f530c
Elindi Absalom <abby141003@gmail.com>
2024-09-26 11:50:06 +0200

    Update

--- AverageSellingPrice.py
+++ AverageSellingPrice.py
@@ -1,16 +1,19 @@
 import matplotlib.pyplot as plt
 import pandas as pd 
+import seaborn as sns 
 
 car_prices_df = pd.read_csv(r'C:\Users\abby1\Documents\DTA Project\car_prices.csv')
 
 # Verifying the variable by showing the first few rows again
 car_prices_df.head()
 # Group the data by 'state' and calculate the average selling price
-state_avg_price = car_prices_df.groupby('state')['sellingprice'].mean().sort_values(ascending=False)
+state_avg_price = car_prices_df.groupby('state')['sellingprice'].mean().sort_values(ascending=False).reset_index()
 
 # Create the bar chart
+sns.set_theme(style="whitegrid")
+
 plt.figure(figsize=(10,6))
-state_avg_price.plot(kind='bar', color='skyblue')
+sns.barplot(x='state', y='sellingprice', data=state_avg_price, palette='Blues_d')
 
 # Add titles and labels
 plt.title('Average Car Selling Price by State', fontsize=14)
